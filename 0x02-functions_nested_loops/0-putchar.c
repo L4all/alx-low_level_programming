6138d014413efbda5f64b53b7dfe995664e62b09
L4all <bennegedu@gmail.com>
2022-09-13 10:16:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print _putchar
+*
+* Description: write a program that prints "_putchar
+* Return: 0
+*/
+int main(void)
+{
+int _putchar
+putchar("%d\n", _putchar);
+return (0);
+}
