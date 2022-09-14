de028c813c916019a6742b6e59796227445e7192
L4all <bennegedu@gmail.com>
2022-09-13 17:59:32 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,15 @@
 * Return: 0
 */
 int main(void)
-char _putchar;
-{
-putchar("_putchar\n");
+{	
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
