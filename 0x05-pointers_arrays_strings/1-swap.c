057520a84acfe1300db616f54fa89ce2cb1d9fa4
L4all <bennegedu@gmail.com>
2022-09-19 19:32:46 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int -Name of the function that will perform the swap
+* @a: The first variable to be swapped
+* @b: The second variable to be swapped
+* Return: void on success
+*/
+void swap_int(int *a, int *b)
+{
+*a += *b;
+*b = *a - *b;
+*a = *a - *b;
+}
