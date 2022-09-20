99ba6d4121413bc64a02413a7e6a4a230bbea0c5
L4all <bennegedu@gmail.com>
2022-09-19 19:59:58 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
-* _put - prints a string followed by a new line..
+* _puts - prints a string followed by a new line..
 *
 * @str: the string to pprint.
-*
+* 
 * Return: void
 */
 void _puts(char *str)
