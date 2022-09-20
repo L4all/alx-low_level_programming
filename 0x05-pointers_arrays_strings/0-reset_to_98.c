90e858625ff136b50256358b41efce44af938ea7
L4all <bennegedu@gmail.com>
2022-09-19 18:30:18 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
-* main reset_to_98 IS THE FUNCTION THAT WILL TAKE AN INTEGER
+* main - check the code  
+* reset_to_98 IS THE FUNCTION THAT WILL TAKE AN INTEGER
 * @n: This is the parameter that will inturn be the arguement
 * Return: void on success
 */
