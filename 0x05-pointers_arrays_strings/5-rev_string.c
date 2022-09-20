15592aa852c8a46c25076af8f0846ccc1d1fa998
L4all <bennegedu@gmail.com>
2022-09-19 20:29:00 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* rev_string - reversw a string
+*
+* @s: the string to be reverseed.
+*/
+
+void rev_string(char *s)
+{
+	int length, j, i;
+	char x1, x2
+		for (length = 0; s[length] != '\0'; length++)
+		{
+		}
+	j = length - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		x1 = s[i];
+		x2 = s[j];
+		s[i] = x2;
+		s[j] = x1;
+		j--;
+		i++
+	}
+}
