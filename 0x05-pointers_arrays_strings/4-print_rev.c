93d4bcaed79ba5530757dfeb8fa15f19434cb735
L4all <bennegedu@gmail.com>
2022-09-19 20:14:16 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_rev - prints astring followed bi a new line..
+*
+* @s: the string to print
+* Return: void
+*/
+void print_rev(char *s)
+{
+	int nu = 0;
+
+	while (s[num])
+	{
+		num++;
+	}
+	while (num--)
+	{
+		putchar(s[num]);
+	}
+	putchar('\n');
+}
