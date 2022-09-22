cafa1c5b9aa09b3b82162f9f340d9923d8432cf6
L4all <bennegedu@gmail.com>
2022-09-22 16:41:24 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,18 @@
 * Works identically to the standard likrary function 'strncpy'.
 * @dest: buffer storing the string copy
 * @src: the source string
-* @n: maximum number odd byte copied
+* @n: maximum number od byte copied
 * Return: returns
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+for ( ; i < n; i++)
+dest[i] = '\0';
 
-	return (dest);
+return (dest);
 }
