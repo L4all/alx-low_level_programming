9da7446d047d5b2f87d95678c6164256c68ab58a
L4all <bennegedu@gmail.com>
2022-11-04 05:42:18 -0700

    function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
 
 /**
- * read_textfile - a function that reads a text file and prints it
- * to POSIX standard output.
+ * read_textfile - a function that reads a text file and prints it to POSIX standard output.
  *
  * @filename: is the file to read
  * @letters: number of letters to read and print from file
- *
  * Return: 0 if it fails or actual number of letters it could
  * read and print
 */
